cd53037e568259ffe0ee7537fe4f3d78d3170564
David <davidcha@email.unc.edu>
2023-12-09 00:16:31 -0500

    started intro javascript module in odinproject

---
+++ odin-foundations/intro-js/js-fundamentals-1/javascript.js
@@ -0,0 +1,31 @@
+// `let` keyword creates a variable
+let message;
+
+message = 'Hello';
+alert(message);
+
+// we can allocate multiple variables on one lien
+let user = 'John', age = 25, speech = 'Hello';
+
+// camelCase is preferred in js?
+
+// constants are immutable
+// UPPERCASE is common practice when naming constants
+const MYBIRTHDAY = '05.13.2002';
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+let color = COLOR_ORANGE;
+alert(color);
+
+let admin;
+let name;
+name = "John";
+admin = name;
+alert(admin);
+
+let ourHomePlanet = "earth";
+let currUserName = "John";
