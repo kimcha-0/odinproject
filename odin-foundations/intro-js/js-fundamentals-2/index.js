0ada8db88a46895cff921e5d6ded18f94948567e
David <davidcha@email.unc.edu>
2023-12-09 14:45:09 -0500

    js-fundamentals 2

---
+++ odin-foundations/intro-js/js-fundamentals-2/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var name = "String";
+// the number type can represent integer and floating point numbers
+// number range is: [-(2^53-1), 2^53-1]
+var num = 123456;
+var bigNum = 1234567890123456789012345678901234567890n;
+console.log("number: {num}");
+console.log(bigNum);
---
+++ odin-foundations/intro-js/js-fundamentals-2/index.ts
@@ -0,0 +1,15 @@
+let name: string = "String";
+
+// the number type can represent integer and floating point numbers
+// number range is: [-(2^53-1), 2^53-1]
+let num: number = 123456;
+
+const bigNum = 1234567890123456789012345678901234567890n;
+console.log("number: {num}" );
+console.log(bigNum);
+
+// alert() is a property of browser `window` objects
+// alert(1 / 0);
+// alert(Infinity);
+
+export {}
