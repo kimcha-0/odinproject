32061447c2c0fc1fdaf8252d4222d59de924a359
David <davidcha@email.unc.edu>
2023-12-09 14:56:52 -0500

    learned javascript data types

--- odin-foundations/intro-js/js-fundamentals-2/index.js
+++ odin-foundations/intro-js/js-fundamentals-2/index.js
@@ -1,9 +1,31 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var name = "String";
-// the number type can represent integer and floating point numbers
-// number range is: [-(2^53-1), 2^53-1]
-var num = 123456;
-var bigNum = 1234567890123456789012345678901234567890n;
-console.log("number: {num}");
-console.log(bigNum);
+let num = 1234;
+let str = "Hello";
+let strTwo = 'Single quotes are okay too';
+let phrase = `can embed another ${str}`;
+
+// booleans
+let nameFieldChecked = true;
+let ageFieldChecked = false;
+
+// booleans can be the result of comparisons
+let isGreater = 4 > 1; // => true
+
+// in JS, null does not indicate a null pointer, it merely represents an "empty" value
+let age = null;
+
+// the "undefined" value indicates that the "value is not assigned"
+let unassigned;
+
+// when we declare a variable without initializing it, it is assigned the "undefined" value
+
+
+// the object Type
+typeof undefined; // undefined
+typeof 0; // number
+typeof 10n; // bigint
+typeof true; // boolean
+typeof "foo"; // string
+typeof Symbol("id"); // symbol type is used to create unique identifiers for objects
+typeof Math; // object
+typeof null; // object
+typeof alert; // function
