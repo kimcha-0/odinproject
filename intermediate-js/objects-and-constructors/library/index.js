c9b58718f5b344937e564e5ea68ec3eacb12ae05
David <davidcha@email.unc.edu>
2023-12-23 22:07:09 -0500

    used dialog to make form appear when button is pressed; need to figure out how to display Books such that the current state is shown without a refresh

--- intermediate-js/objects-and-constructors/library/index.js
+++ intermediate-js/objects-and-constructors/library/index.js
@@ -15,26 +15,52 @@ class Book {
     }
 }
 
-myLibrary.push(Book('the Idiot', 'Dostoevsky', 613, 'read'));
-myLibrary.push(Book('Crime and Punishment', 'Dostoevsky', 600, 'read'));
+myLibrary.push(new Book('the Idiot', 'Dostoevsky', 613, 'read'));
+myLibrary.push(new Book('Crime and Punishment', 'Dostoevsky', 600, 'read'));
 
 // submit button from form
 const button = document.querySelector('button');
-button.addEventListener("click", (title, author, pages, read) => {
+
+const dialog = document.querySelector("dialog");
+const addBookButton = document.querySelector("dialog + button")
+const submitButton = document.querySelector("button#add-book");
+
+
+addBookButton.addEventListener("click", () => dialog.showModal());
+
+submitButton.addEventListener("click", () => {
+    dialog.closeModal();
+    event.preventDefault();
+});
+
+function addBook() {
+
     const newBookObj = new Book(title, author, pages, read);
     myLibrary.push(newBookObj);
     console.log(`book ${newBookObj} added`);
-});
+}
+
 
 
 // remove book function
 // builder pattern would be suitable for this problem?
 function displayBooks() {
     // add books to HTML Display in their own Card
-    // const booksContainer  = document.querySelector('.book-shelf');
+    const booksContainer  = document.querySelector('.book-shelf');
     for (let i = 0; i < myLibrary.length; i++) {
-        console.log(typeof myLibrary[i]);
+        const bookElement = document.createElement('div');
+        bookElement.className = 'book-card';
+        bookElement.id = i;
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Remove Book';
+        deleteButton.addEventListener("click", () => {
+            console.log('remove');
+            delete myLibrary[i];
+        });
+        bookElement.appendChild(deleteButton);
+        booksContainer.appendChild(bookElement);
     }
+    console.log(myLibrary);
 }
 
 displayBooks();
