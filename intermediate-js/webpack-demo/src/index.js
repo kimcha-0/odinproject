f215a46c68a69137c66c59248d0c8a6fd91654c2
David <davidcha@email.unc.edu>
2023-12-25 14:57:44 -0500

    learning webpack bundler and git ignoring node modules

---
+++ intermediate-js/webpack-demo/src/index.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+import myName from './myName';
+
+function component() {
+    const element = document.createElement('div');
+    element.textContent = myName('David');
+
+    return element;
+}
+
+document.body.appendChild(component());
+
