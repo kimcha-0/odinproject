d82971f0082716dff34f6846f7cf7face8e1671a
David <davidcha@email.unc.edu>
2024-01-01 21:07:43 -0500

    I think the game logic works, need to link it to the UI

--- intermediate-js/js-modules/tictactoe/index.js
+++ intermediate-js/js-modules/tictactoe/index.js
@@ -4,10 +4,11 @@ const GameBoard = (() => {
     for (i = 0; i < 3; i++) {
         const row = [];
         for (j = 0; j < 3; j++) {
-            row.push(0);
+            row.push('');
         }
         board.push(row);
     }
+
 
     const getBoard = () => board;
 
@@ -30,69 +31,10 @@ const GameBoard = (() => {
     };
 
     const changeBoardState = (x, y, newValue) => {
-        grid[x][y] = newValue;
+        board[x][y] = newValue;
     }
 
-    return {
-        getBoard,
-        checkWinCondition,
-        changeBoardState,
-    }
-
-})();
-
-const DisplayBoard= (() => {
-    const gridCells = document.querySelectorAll('grid-cell');
-    const board = 
-    const display = () => {
-        count = 0;
-        for (i = 0; i < board.length; i++) {
-            for ( j = 0; j < board[i].length; j++) {
-                if (board[i][j] === 1) {
-                    gridCells[count].textContent = 'X';
-                } else if (board[i][j] === 2) {
-                    gridCells[count].textContent = 'O';
-                }
-                gridCells[count].addEventListener('click', () => {
-                    // call display controller method and send coordinate data
-                    DisplayBoardController.changeBoardController(i, j, player);
-                });
-                count += 1;
-            }
-        }
-    };
-
-    const clearDisplay = () => {
-        for (i = 0; i < gridCells.length; i++) {
-            gridCells[i].textContent = '';
-        }
-    }
-
-    return {
-        display,
-        clearDisplay,
-    }
-
-})();
-
-const Player = (type) => {
-    const getPlayerType = () => type;
-
-    const playMove = (x, y) => {
-        DisplayBoardController.changeBoardController(x, y, type);
-    }
-
-    return {
-        getPlayerType,
-        playMove,
-    }
-};
-
-const DisplayBoardController = (() => {
-    const changeBoardController = (x, y, player) => {
-        GameBoard.changeBoardState(x, y, player);
-
-    }
+    const cellIsEmpty = (x, y) => board[x][y] === '';
 
     const renderDisplay = () => {
         Display.clearDisplay();
@@ -100,18 +42,90 @@ const DisplayBoardController = (() => {
     }
 
     return {
-        changeBoardController,
+        getBoard,
+        checkWinCondition,
+        changeBoardState,
         renderDisplay,
+        cellIsEmpty,
     }
 
 })();
 
-const xPlayer = Player('X');
-const yPlayer = Player('Y');
-let turn = true;
+const Display = (() => {
+    const boardEl = document.querySelectorAll('.board-cell');
 
-turn = false;
+    const renderDisplay = (board) => {
+        let count = 0;
+        for (i = 0; i < board.length; i++) {
+            for ( j = 0; j < board[i].length; j++) {
+                boardEl[count].textContent = board[i][j];          
+                count++;
+            }
+        }
+    }
 
-const body = document.querySelector('body');
-const playGameCtrl = document.querySelector('game-controls');
-const grid = GameBoard;
+    const clearDisplay = () => {
+        for (i = 0; i < boardEl.length; i++) {
+            boardEl[i].textContent = '';
+        }
+    }
+
+
+    return {
+        renderDisplay,
+        clearDisplay,
+    }
+})();
+
+const Game = () => {
+    let numMoves = 0;
+    let turn = true;
+    const inputValidation = (x, y) => x >= 0 && x < 3 && y >= 0 && y < 3 && GameBoard.cellIsEmpty(x, y);
+    while (true) {
+
+        if (turn) {
+            // X's turn
+            let xXMove = prompt("X move: Enter an X Coordinate: ") - 1;
+            let yXMove = prompt("X move: Enter an Y Coordinate: ") - 1;
+            console.log(inputValidation(xXMove, yXMove));
+            while (!inputValidation(xXMove, yXMove)) {
+                xXMove = prompt("X move: Enter an X Coordinate: ") - 1;
+                yXMove = prompt("X move: Enter an Y Coordinate: ") - 1;
+                console.log(inputValidation(xXMove, yXMove));
+            }
+            GameBoard.changeBoardState(xXMove, yXMove, 'X');
+            Display.renderDisplay(GameBoard.getBoard());
+
+            if (GameBoard.checkWinCondition('X') === true) {
+                alert('X wins!');
+                break;
+            }
+            turn = false;
+        } else {
+            let xOMove = prompt("O move: Enter an X Coordinate: ") - 1;
+            let yOMove = prompt("O move: Enter an Y Coordinate: ") - 1;
+            console.log(inputValidation(xOMove, yOMove));
+            while (!inputValidation(xOMove, yOMove)) {
+                xOMove = prompt("O move: Enter an X Coordinate: ") - 1;
+                yOMove = prompt("O move: Enter an Y Coordinate: ") - 1;
+                console.log(inputValidation(xOMove, yOMove));
+            }
+            GameBoard.changeBoardState(xOMove, yOMove, 'O');
+            Display.renderDisplay(GameBoard.getBoard());
+
+            if (GameBoard.checkWinCondition('O') === true) {
+                alert('O wins!');
+                break;
+            }
+            // O's turn
+            turn = true;
+        }
+        numMoves++;
+        if (numMoves === 9) {
+            alert('tie!');
+            break;
+        }
+    }
+};
+
+Game();
