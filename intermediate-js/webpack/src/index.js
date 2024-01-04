4002c1a8c7af55a668eb7384457ada6d43189ead
David <davidcha@email.unc.edu>
2024-01-04 16:07:53 -0500

    finished basic webpack tutorial; I want to learn the concepts of webpack more in depth after working on a couple projects

--- intermediate-js/webpack/src/index.js
+++ intermediate-js/webpack/src/index.js
@@ -1,7 +1,15 @@
-function component() {
-    const element = document.createElementById('div');
+import _ from 'lodash';
+import './style.css';
+import printMe from './print.js';
 
+function component() {
+    const element = document.createElement('div');
+    const button = document.createElement('button');
+    button.innerHtml = 'Click me';
+    button.addEventListener('click', () => printMe());
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+    element.appendChild(button);
 
     return element;
 }
