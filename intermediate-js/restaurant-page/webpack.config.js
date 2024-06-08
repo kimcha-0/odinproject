2ba45882e78b6539878a9ea2f45e6af596794c0e
David <davidcha@email.unc.edu>
2024-06-08 12:51:34 -0400

    setup webpack.config.js and basic skeleton files

--- intermediate-js/restaurant-page/webpack.config.js
+++ intermediate-js/restaurant-page/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
 	entry: {
@@ -6,15 +7,34 @@ module.exports = {
 	},
 	mode: 'development',
 	devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: './src/index.html'
-			name: 'Restaurant Page',
+			template: './src/index.html',
+			title: 'Restaurant Page',
 		}),
 	],
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist'),
 		clean: true,
+        publicPath: '/',
 	},
+    optimization: {
+        runtimeChunk: 'single',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 };
